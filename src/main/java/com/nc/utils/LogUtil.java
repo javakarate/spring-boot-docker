515a1f3bba9bc3b2178fc0e8fb9e38991586c4b8
Jose Alberto Romero Jimenez <jarj@netcompany.com>
2019-11-06 16:16:58 +0100

    Modified LogUtil

--- src/main/java/com/nc/utils/LogUtil.java
+++ src/main/java/com/nc/utils/LogUtil.java
@@ -2,6 +2,9 @@ package com.nc.utils;
 
 import java.util.Date;
 
+/**
+* Util class for logging to be used in logging purposes
+**/
 public class LogUtil {
 
     public static String getCurrentDateTime() {
